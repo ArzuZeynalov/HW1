916b9795d963a78e2d0fa72b6cdb888c944ee94d
ArzuZeynalov <lokoarzu@gmail.com>
2021-04-23 02:27:20 +0300

    Initial commit

---
+++ src/test/scala/hw1test.scala
@@ -0,0 +1,11 @@
+import org.scalatest.FunSuite
+
+class hw1test extends FunSuite {
+
+  test ("hw1") {
+    assert(hw1.div(3,21) === Vector(3, 6, 7, 9, 12, 14, 15, 18, 21))
+  }
+
+
+
+}
---
+++ src/test/scala/hw2test.scala
@@ -0,0 +1,11 @@
+import org.scalatest.FunSuite
+
+class hw2test extends FunSuite {
+
+  test(testName = "hw2"){
+    assert(hw2.sumOfDivBy3Or(3,8) === 14)
+  }
+
+
+
+}
